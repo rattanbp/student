3fd6265d9382ff3c69bb67bda7c2da0935b52076
rattanbp <payalrbhardhwaj@gmail.com>
2019-05-17 14:41:00 -0400

    2 commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -14,7 +14,8 @@ public class StudentList {
     public static void main(String[] args) {
         
         Student [] sList= new Student[5];
-        
+    
+    
         for (int i=0;i<sList.length;i++)
         {
             sList[i]=new Student();
